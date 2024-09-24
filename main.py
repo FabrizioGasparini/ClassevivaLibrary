b0c7d7c1c60f1e903f67e0e5b051c14066f2346f
Fabrizio Gasparini <makerfaffa@gmail.com>
2024-09-24 08:46:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ utente.login()
 #subjects = utente.request(RequestURLs.AGENDA_URL, ("20240919", "20240925"))
 #utente.print_prettify_output(subjects)
 
+azioni = {"bacheca": RequestURLs.NOTICE_BOARD_URL, "materie": RequestURLs.SUBJECTS_URL, "lezioni": RequestURLs.TODAY_LESSONS_URL, "card": RequestURLs.CARD_URL, "libri":RequestURLs.SCHOOLBOOKS_URL}
+
 inp = " "
 while inp != "":
     inp = input("Azione: ")
     richiesta = utente.request(azioni[inp])
     utente.print_prettify_output(richiesta)
     
-
-azioni = {"bacheca": RequestURLs.NOTICE_BOARD_URL, "materie": RequestURLs.SUBJECTS_URL, "lezioni": RequestURLs.TODAY_LESSONS_URL, "card": RequestURLs.CARD_URL, "libri":RequestURLs.SCHOOLBOOKS_URL}
