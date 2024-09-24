934831ebc182124e084327f9db017192fcf2ba72
Fabrizio Gasparini <makerfaffa@gmail.com>
2024-09-24 08:32:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ from classeviva import User, RequestURLs
 
 utente = User("G10409310F", "Fabrizio2008#")
 utente.login()
-subjects = utente.request(RequestURLs.AGENDA_URL, ("20240919", "20240925"))
-utente.print_prettify_output(subjects)
+#subjects = utente.request(RequestURLs.AGENDA_URL, ("20240919", "20240925"))
+#utente.print_prettify_output(subjects)
+
+inp = " "
+while inp != "":
+    inp = input("Azione: ")
+    richiesta = utente.request(azioni[inp])
+    utente.print_prettify_output(richiesta)
+    
+
+azioni = {"bacheca": RequestURLs.NOTICE_BOARD}
